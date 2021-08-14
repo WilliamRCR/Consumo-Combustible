f330b054b4ad2b7874fbdc37d1d7bc1ac21099af
WilliamRCR <williamcastanazaromero2@gmail.com>
2021-08-14 15:03:24 -0600

    first commit

---
+++ Consumo.py
@@ -0,0 +1,5 @@
+print("Consumo de combustible")
+k = float(input("Ingrese el numero de kilometros recorridos"))
+l = float(input("Ingrese la cantidad de litros de combustible utilizados"))
+consumo = k/l
+print ("El consumo por kilometro es de ", consumo, "litros")
